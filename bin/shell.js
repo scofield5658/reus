bd6e98944e10a1ff3c898544e1043a3f5993911b
guozhi <zhi.guo@geekthings.com.cn>
2019-06-18 14:17:43 +0800

    [update] shell.js

--- bin/shell.js
+++ bin/shell.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
 require('babel-register');
 require('babel-polyfill');
-require('babel-core')
-  .transform('code', {
-    presets: [ [ require( 'babel-preset-latest-node' ), {
-      target: 'current'
-    } ] ]
-  } );
 require('../src/command');
